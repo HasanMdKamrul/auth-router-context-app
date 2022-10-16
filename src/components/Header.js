b6c092b1de5f452aa7083cce41dc0a7e67b99fb2
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-16 04:35:19 +0200

    initial rendering of login and register page added

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     <div>
       <div className="navbar bg-base-300">
         <NavLink to="/" className="btn btn-ghost normal-case text-xl">daisyUI</NavLink>
+        <NavLink className='mr-2 btn btn-ghost normal-case text-xl' to="/home">Home</NavLink>
         <NavLink className='mr-2 btn btn-ghost normal-case text-xl' to="/login">Log in</NavLink>
         <NavLink className=' btn btn-ghost normal-case text-xl' to="/Register">Register</NavLink>
       </div>
